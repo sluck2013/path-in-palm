cfe45eb25bcc547cdeb30cd99e687bb3dc8969db
Jenson <sluck1988@gmail.com>
2016-07-02 23:35:17 -0400

    map filter functionalities

--- app/src/main/java/com/yansong_wang/pathinpalm/MainActivity.java
+++ app/src/main/java/com/yansong_wang/pathinpalm/MainActivity.java
@@ -65,12 +65,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.map_options, menu);
-        return true;
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
--- app/src/main/java/com/yansong_wang/pathinpalm/MapFragment.java
+++ app/src/main/java/com/yansong_wang/pathinpalm/MapFragment.java
@@ -5,8 +5,11 @@ import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
 import android.support.design.widget.CoordinatorLayout;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,37 +17,57 @@ import android.widget.ImageView;
 
 public class MapFragment extends Fragment {
 
+    private CoordinatorLayout fragmentView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        CoordinatorLayout layout = (CoordinatorLayout) inflater.inflate(R.layout.map_wrapper_view, container, false);
-        loadMap(layout);
-        return layout;
+        setHasOptionsMenu(true);
+        fragmentView = (CoordinatorLayout) inflater.inflate(R.layout.map_wrapper_view, container, false);
+        loadMap(R.drawable.pathmap_weekend, R.drawable.pathmap_weekday);
+        return fragmentView;
     }
 
-    private void loadMap(final View view) {
-        ImageView mapImageView = (ImageView) view.findViewById(R.id.img_view_map);
-        Drawable[] mapLayers = loadMapLayers();
+    private void loadMap(final @DrawableRes int... mapImageResourceIds) {
+        ImageView mapImageView = (ImageView) fragmentView.findViewById(R.id.img_view_map);
+        Drawable[] mapLayers = loadMapLayers(mapImageResourceIds);
         LayerDrawable layerDrawable = new LayerDrawable(mapLayers);
         mapImageView.setImageDrawable(layerDrawable);
     }
 
-    private Drawable[] loadMapLayers() {
+    private Drawable[] loadMapLayers(final @DrawableRes int... mapImageResourceIds) {
         Resources resources = getResources();
-        Drawable[] layers = new Drawable[2];
-        layers[0] = resources.getDrawable(R.drawable.pathmap_weekend);
-        layers[1] = resources.getDrawable(R.drawable.pathmap_weekday);
+        Drawable[] layers = new Drawable[mapImageResourceIds.length];
+        int index = 0;
+        for (final int resourceId : mapImageResourceIds) {
+            layers[index++] = resources.getDrawable(resourceId);
+        }
         return layers;
     }
 
 
     @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.map_options, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.options_menu_item_map_weekday) {
-            return true;
+        switch (id) {
+            case R.id.options_menu_item_map_weekday:
+                loadMap(R.drawable.pathmap_weekday);
+                break;
+            case R.id.options_menu_item_map_weekend:
+                loadMap(R.drawable.pathmap_weekend);
+                break;
+            case R.id.options_menu_item_map_show_all:
+                loadMap(R.drawable.pathmap_weekend, R.drawable.pathmap_weekday);
+                break;
+            default:
         }
-
         return super.onOptionsItemSelected(item);
     }
 
